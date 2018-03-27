b382f1731f8bf7677b9003c975d58fb9df6ebda0
andreaangiolillo <angiolillo.andrea@gmail.com>
2018-03-27 02:34:09 +0200

    creating permissions request

--- app/src/main/java/com/app/lucky/songsfixer/MainActivity.kt
+++ app/src/main/java/com/app/lucky/songsfixer/MainActivity.kt
@@ -1,17 +1,57 @@
 package com.app.lucky.songsfixer
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
+val PERMISSIONS_MULTIPLE_REQUEST = 123
 class MainActivity : AppCompatActivity() {
 
+
+
+
+// see the following video to complete show the list of songs: https://www.youtube.com/watch?v=kf2fxYLOiSo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) +
+                ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE ),
+                        PERMISSIONS_MULTIPLE_REQUEST)
+
+                // PERMISSIONS_MULTIPLE_REQUEST is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+
     }
 
     /**
